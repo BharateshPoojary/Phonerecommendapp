a59769fe893261af98cbcd8b7c1d38da7da0d56d
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-03-25 15:12:20 +0530

    some changes

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 
 window.addEventListener("load",async()=>{
+    let osmenu=document.getElementById("osmenu");
+    osmenu.style.display="none";
+   let togglearrow=document.getElementById("arrow");
+   togglearrow.addEventListener("click",()=>{
+    if ( togglearrow.style.transform==="rotate(0deg)") {
+        togglearrow.style.transform="rotate(180deg)";
+        osmenu.style.display="block";
+    }else{
+        togglearrow.style.transform="rotate(0deg)";
+        osmenu.style.display="none";
+    }
+   
+   })
     let latestphonedata= await(await fetch("http://localhost/MYPROJECTLOGINPAGE/phonedata/phoneapi.php")).json();
     console.log(latestphonedata);
 
 let divforcard=document.createElement("div");
-divforcard.style.display="grid";
-divforcard.style.gridTemplateColumns="repeat(3,1fr)"
-divforcard.style.gap="3px";
+divforcard.style.display="flex";
+divforcard.style.flexDirection="column";
+// divforcard.style.overflow="hidden";
+divforcard.id="mobilecontainer";
+
+divforcard.style.marginLeft="230px";
+
 document.body.appendChild(divforcard)
 
 latestphonedata.forEach(obj => {
     let cardforeachphone=document.createElement("div");
-    cardforeachphone.style.height="260px";
-    cardforeachphone.style.width="350px";
+    cardforeachphone.style.height="300px";
+    cardforeachphone.style.width="71rem";
     cardforeachphone.style.border="2px solid black";
     cardforeachphone.style.display="flex";
+    cardforeachphone.style.marginTop="5px";
     cardforeachphone.style.flexDirection="row"
     divforcard.appendChild(cardforeachphone);
     let phoneimage=document.createElement("img");
     phoneimage.src=obj.PhoneImageUrl;
+    phoneimage.style.height="296px";
     cardforeachphone.appendChild(phoneimage);
     console.log(obj.PhoneImageUrl);
     let phonename=document.createElement("h2");
